da8f03161606d85f331e2186f76058d66aa47609
Jennifer Uribe <138295617+jennieub@users.noreply.github.com>
2024-11-18 15:50:07 -0400

    login sin internet

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,15 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cep-azul': '#0D0AE3', // 'azul-cmyk': 'cmyk(95%, 96%, 11%, 0%)',
+        'cep-rojo': '#B30000', // 'rojo-cmyk': 'cmyk(30%, 100%, 100%, 2%)',
+        'cep-violeta': '#38039B', // 'violeta-cmyk': 'cmyk(78%, 99%, 38%, 4%)',
+        'cep-vino': '#820039', // 'vino-cmyk': 'cmyk(44%, 100%, 71%, 14%)',
+        'cep-amarillo': '#ED9F29', // 'amarillo-cmyk': 'cmyk(7%, 44%, 84%, 0%)'
+      }
+    },
   },
   darkMode: "class",
   plugins: [nextui()]
